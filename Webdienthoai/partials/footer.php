c05e4ed3b639bfac09a3a4c3af239011a5ed1749
Khanh Quang <khanhquang213@gmail.com>
2024-05-10 23:05:05 +0700

    update webdienthoai

---
+++ Webdienthoai/partials/footer.php
@@ -0,0 +1,10 @@
+ <!-- cuoi -->
+ <div class="header">
+     <div class="header-container">
+         <header class="header-bottom">
+             <span>2024 © QQT Design. All rights reserved. Developed by QQT Software.
+             </span>
+         </header>
+     </div>
+     <div class="container-botom"></div>
+ </div>
